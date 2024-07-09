8381570b1d0941d4dcc07d153af5a57801c934f4
OussaMac <Oussama@OussaMac.lan>
2024-07-09 19:05:41 +0200

    update generation script to generate hourly visitor captured by each sensor

--- src/data/script_generate_data.py
+++ src/data/script_generate_data.py
@@ -11,31 +11,32 @@ store_locations = ["Lille", "Paris", "Toulouse"]
 business_year = 2024
 months_range = [i for i in range(1, 13)]
 days_range = [i for i in range(1, 29)]
-business_hour = "12:00"
+hours_range = [f"0{i}:00" for i in range(10)] + [f"{i}:00" for i in range(10, 24)]
 
 for store_sensor in store_locations:
-    for id_sensor in range(1, 9):
+    for id_sensor in range(1, 4):
         for business_month in months_range:
             for business_day in days_range:
+                for business_hour in hours_range:
 
-                business_date = f"{business_day}-{business_month}-{business_year}"
-                # comparing date selected with today
-                if (
-                    datetime.strptime(business_date, "%d-%m-%Y").date()
-                    <= datetime.now().date()
-                ):
-                    number_visitors = retrieve_visitor_api(
-                        store_sensor, id_sensor, business_date, business_hour
-                    )
-                    sensor_data.append(
-                        [
-                            store_sensor,
-                            id_sensor,
-                            business_date,
-                            business_hour,
-                            number_visitors,
-                        ]
-                    )
+                    business_date = f"{business_day}-{business_month}-{business_year}"
+                    # comparing date selected with today
+                    if (
+                        datetime.strptime(business_date, "%d-%m-%Y").date()
+                        <= datetime.now().date()
+                    ):
+                        number_visitors = retrieve_visitor_api(
+                            store_sensor, id_sensor, business_date, business_hour
+                        )
+                        sensor_data.append(
+                            [
+                                store_sensor,
+                                id_sensor,
+                                business_date,
+                                business_hour,
+                                number_visitors,
+                            ]
+                        )
 
 
 # Specify the file path
